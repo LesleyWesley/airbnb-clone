94db31b02e3a57b532ef0d6ca52e5bedd0af09e6
lesley_w <lrwesley92@gmail.com>
2023-10-07 17:19:53 -0500

    Added logic for sold out and online badges, and added object props intead of individual props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,25 @@ import React from "react"
 import starIcon from "../images/star.png"
 
 function Card(props) {
+  let badgeText
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE"
+  }
+
   return (
       <article className="card">
-        <img src={props.img} className="card--image"/>
+        {badgeText && <div className="card--badge">{badgeText}</div>}
+        <img src={props.item.coverImg} className="card--image"/>
         <div className="card--stats">
           <img src={starIcon} alt="star icon" className="card--star" />
-          <span>{props.rating}</span>
-          <span className="grey">({props.reviewCount}) •</span>
-          <span className="grey">{props.country}</span>
+          <span>{props.item.stats.rating}</span>
+          <span className="grey">({props.item.stats.reviewCount}) •</span>
+          <span className="grey">{props.item.location}</span>
         </div>
-        <p className="card--title">{props.title}</p>
-        <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        <p className="card--title">{props.item.title}</p>
+        <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
       </article>
   )
 }
